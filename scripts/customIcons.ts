3de07c146c741753900f0bac9e1b80a57a8f7de1
Otto Streifel <ottost@microsoft.com>
2017-05-09 10:36:40 -0700

    Fix import errors for officefabric

--- scripts/customIcons.ts
+++ scripts/customIcons.ts
@@ -1,3 +1,5 @@
+import { Dialog, DialogFooter, DialogType } from "OfficeFabric/components/Dialog";
+
 for (const iconElem of $.makeArray($(".work-item-type-icon:not(.custom-icon)"))) {
     const iconJquery = $(iconElem);
     iconJquery.addClass("custom-icon");
