33ef262eb0d3622c990d96d3a172d6c5b0bd2955
Otto Streifel <ottost@microsoft.com>
2017-05-10 11:54:28 -0700

    dialog stub

--- scripts/customIcons.tsx
+++ scripts/customIcons.tsx
@@ -1,16 +1,49 @@
 import { Dialog, DialogFooter, DialogType } from "OfficeFabric/components/Dialog";
+import { PrimaryButton, DefaultButton } from "OfficeFabric/components/Button";
 import * as React from "react";
 import * as ReactDom from "react-dom";
 
 const dialogContainer = $("<div></div>");
 $("body").append(dialogContainer[0]);
 
-class SelectIconDialog extends React.Component<{}, {}> {
+class SelectIconDialog extends React.Component<{}, { showDialog: boolean }> {
+    constructor() {
+        super();
+        this.state = {
+            showDialog: true
+        };
+    }
+
     public render() {
-        return <div>{this.props.children}</div>;
+        return (
+            <div>
+                <Dialog
+                    isOpen={this.state.showDialog}
+                    type={DialogType.normal}
+                    onDismiss={this._closeDialog.bind(this)}
+                    title='All emails together'
+                    subText='Your Inbox has changed. No longer does it include favorites, it is a singular destination for your emails.'
+                    isBlocking={false}
+                    containerClassName='ms-dialogMainOverride'
+                >
+                    {this.props.children}
+                    <DialogFooter>
+                        <PrimaryButton onClick={this._closeDialog.bind(this)} text="Save" />
+                        <DefaultButton onClick={this._closeDialog.bind(this)} text="Cancel" />
+                    </DialogFooter>
+                </Dialog>
+            </div>
+        );
+    }
+
+    private _showDialog() {
+        this.setState({ showDialog: true });
+    }
+
+    private _closeDialog() {
+        this.setState({ showDialog: false });
     }
 }
-ReactDom.render(<SelectIconDialog />, dialogContainer[0]);
 function inputIcon(classes) {
     ReactDom.render(<SelectIconDialog>{`Icon Clicked ${classes}`}</SelectIconDialog>, dialogContainer[0]);
 }
